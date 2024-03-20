d10af85c013e49cf43250da539e8296988afc14c
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-03-20 06:05:30 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,7 +31,29 @@ int main(){
     };
     
     cout << "PROBLEM 3\n";
+    double num1=0;
+    double num2=0;
+    double num3=0;
+    cin>>num1>>num2>>num3;
+    if (num1>num2&&num1>num3){
+        cout<<num1<<" is the largest"<<endl;
+    } else if(num2>num3){
+        cout<<num2<<" is the largest"<<endl;
+    } else{
+        cout <<num3<<" is the largest"<<endl;
+    };
+    
     cout << "PROBLEM 4\n";
+
+    int num1=0;
+    int num2=0;
+    cin>>num1>>num2;
+    if (num1%num2==0){
+        cout<<num1<<" is divisible by "<<num2<<endl;
+    }else{
+        cout<<num1<<" is not divisible by "<<num2<<endl;
+    };
+    
     cout << "PROBLEM 5\n";
     cout << "PROBLEM 6\n";
     cout << "PROBLEM 7\n";
