d094fb8b59a8910bec6b15b59606a28541e6edcc
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-03-20 05:53:30 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,47 @@ int main(){
     };
     return 0;
 
+    cout << "PROBLEM 2\n";
+
+    int x=0;
+    int rem=0;
+    cin>>x;
+    rem=x%2;
+    if (x>0 && rem==0){
+        cout<<"The number is positive and even";
+    } else if(x>0&&rem!=0){
+        cout<<"The number is positive and odd";
+    } else if(x<0&&rem==0){
+        cout <<"The number is negative and even";
+    }
+    else{
+        cout<<"The number is negative and odd";
+    };
+    
+    cout << "PROBLEM 3\n";
+    cout << "PROBLEM 4\n";
+    cout << "PROBLEM 5\n";
+    cout << "PROBLEM 6\n";
+    cout << "PROBLEM 7\n";
+    cout << "PROBLEM 8\n";
+    cout << "PROBLEM 9\n";
+    cout << "PROBLEM 10\n";
+    cout << "PROBLEM 11\n";
+    cout << "PROBLEM 12\n";
+    cout << "PROBLEM 13\n";
+    cout << "PROBLEM 14\n";
+    cout << "PROBLEM 15\n";
+    cout << "PROBLEM 16\n";
+    cout << "PROBLEM 17\n";
+    cout << "PROBLEM 18\n";
+    cout << "PROBLEM 19\n";
+    cout << "PROBLEM 20\n";
+    cout << "PROBLEM 21\n";
+    cout << "PROBLEM 22\n";
+    
+    
+        
+
 
 
 
