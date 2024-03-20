6acca09f7df42f06e55d040ead1b9e5c8ffb2936
Adxamjon05 <161422394+Adxamjon05@users.noreply.github.com>
2024-03-20 10:28:07 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -70,16 +70,7 @@ int main(){
     
     cout << "PROBLEM 6\n";
 
-    double a6=0;
-    double b6=0;
-    double c6=0;
-    cin>>a6>>b6>>c6;
-    double x1=0;
-    double x2=0;
-    x1=(-b6+sqrt(pow(b6,2)-4*a6*c6))/(2*a6);
-    x2=(-b6-sqrt(pow(b6,2)-4*a6*c6))/(2*a6);
-    cout<<"x1="<<x1<<endl;
-    cout<<"x2="<<x2<<endl;
+    
 
 
     
